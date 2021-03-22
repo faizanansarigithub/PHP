4f9aec37c487bbfc7f5cca6bac68c2a46cdd94f8
faizanansarigithub <77899660+faizanansarigithub@users.noreply.github.com>
2021-03-22 10:42:28 +0530

    DynamicCertificate

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+header('content-type:image/jpeg');
+$font=realpath('BRUSHSCI.TTF');
+$image=imagecreatefromjpeg("certificate.jpg");
+$color=imagecolorallocate($image,19,21,22);
+$date=date('d F,Y');
+$name="Faizan Ansari";
+imagettftext($image,40,0,400,420,$color,$font,$name);
+imagettftext($image,20,0,450,600,$color,$font,$date);
+imagejpeg($image);
+
+imagedestroy($image);
+
+
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<form action="">
+	<h1>WelCome To Engineerng Institute Raipur(C.G)</h1>
+	
+	
+	</form>
+</body>
+</html>
